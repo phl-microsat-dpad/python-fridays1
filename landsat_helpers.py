2fadb8add98066581d678e6a6ef51cc95ff5f068
lululelu <jerine.amado@gmail.com>
2016-01-22 16:52:48 +0800

    git daisuki des

--- landsat_helpers.py
+++ landsat_helpers.py
@@ -1,7 +1,6 @@
 def get_scene_details (scene):		
 	
-	return {'path': scene[3:6] , 
-	        'year': scene[9:13], 
+	return {'path': scene[3:6] , 'year': scene[9:13], 
 			'day': scene[13:16], 
 			'row' : scene[6:9]}
 
